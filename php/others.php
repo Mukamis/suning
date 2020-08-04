f126626a170eef7df8bb12df9b4a034c86a46415
Mukamis <1208867491@qq.com>
2020-08-04 08:50:07 +0800

    八月三日添加了详情页页面渲染和相关链接的跳转

--- php/others.php
+++ php/others.php
@@ -1,8 +1,16 @@
 <?php
-    $sid=$_POST['sid'];
-    $sql="select * from phone where sid='$sid";
-    $result=$conn->query($sid);
-    $arr=[];
+    include "conn.php";
+
+    $id=$_POST['id'];
+    $sql1="select sid from phone where id='$id'";
+    // echo $sql1;
+    $result1=$conn->query($sql1);
+    $result2=$result1->fetch_assoc();
+    $sid=$result2['sid'];
+    $sql="select * from phone where sid='$sid'";
+    // echo $sql;
+    $result=$conn->query($sql);
+    $arr=array();
     for($i=0;$i<$result->num_rows;$i++){
         $arr[$i]=$result->fetch_assoc();
     }
--- src/js/details.js
+++ src/js/details.js
@@ -5,6 +5,7 @@ define([], function() {
             let $id = location.search.substring(1).split('=')[1];
             let showwrap = $('.show-wrap');
             console.log($id);
+            var $sid;
             $.ajax({
                 type: 'get',
                 url: 'http://localhost/suning/php/one.php',
@@ -12,37 +13,61 @@ define([], function() {
                     id: $id
                 }
             }).then(function(data) {
-                console.log(data);
-                // let htmlstr = '';
+                // console.log(data);
+                let htmlstr = '';
                 let arr = JSON.parse(data);
                 // console.log(arr);
-                console.log(arr.title);
-                console.log($('.small img'));
+                // console.log(arr.title);
+                // console.log($('.small img'));
                 $('.small img').prop('alt', arr.title);
                 // console.log(arr.img);
                 $('.small img').prop('src', arr.img);
-                console.log($('.small img').src);
+                // console.log($('.small img').prop('src'));
                 $('.show-title h2').innerHTML = "<span>" + arr.retailer + "</span>" + arr.title;
-                // $('.price-left dl:nth-type-of(0)')
-                // let arr1 = arr.server.split(';');
-                // $.each(arr1, function(index, value) {
-                //     htmlstr += `${value}<input type='checkbox' name='server${index}'>`;
-                // });
-                // let $sid = arr.sid;
+                let arr1 = arr.server.split(';');
+                $.each(arr1, function(index, value) {
+                    htmlstr += `${value}<input type='checkbox' name='server${index}'>`;
+                });
+                $('.show-price>dl:nth-of-type(6) p').html(htmlstr);
+
+                // $sid = arr.sid;
                 // console.log($sid);
-                // $.ajax({
-                //     type: 'post',
-                //     url: 'http://localhost/suning/php/others.php',
-                //     data: {
-                //         sid: $sid
-                //     }
-                // }).done(function(data) {
-                //     let arr2 = json.parse(data);
-                //     $.each(arr2, function(index, value) {
-                //         htmlstr += `${value}`;
-                //     });
-                // });
-                // showwrap.html(htmlstr);
+
+            });
+            let picstr = '';
+            let colorstr = '';
+            $.ajax({
+                type: 'post',
+                url: 'http://localhost/suning/php/others.php',
+                data: {
+                    id: $id
+                }
+            }).done(function(data) {
+                console.log(data);
+                let arr2 = JSON.parse(data);
+                console.log(arr2);
+                $.each(arr2, function(index, value) {
+                    picstr += `<li><img src="${value.img}" alt=""></li>`;
+                    colorstr += `
+                    <a href="http://localhost/suning/src/details.html?id=${value.id}">
+                        <img width="40" height="40" src="${value.img}" alt="">
+                        <span>${value.colors}</span>
+                    </a>`;
+                });
+                console.log(picstr);
+                $('.show-price>dl:nth-of-type(4) p').html(colorstr);
+                $('.picurl>nav').html(picstr);
+            });
+
+            //数量的加减
+            let jia = $('.jia');
+            let num = $('.num');
+            console.log(num);
+            $('.jian').on('click', function() {
+                console.log(this);
+                console.log($('.num'));
+                console.log($('.num').val());
+                $('.num').val() ++;
             });
         }
     }
